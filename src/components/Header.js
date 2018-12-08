87e3f76f1423747a083140732ba187bbb556a815
pluwum <luwyxx@gmail.com>
2018-12-08 05:18:00 +0300

    Change folder structure.

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='App-header'>
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className='App-link'
+        href='https://reactjs.org'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        Learn React
+      </a>
+    </header>
+  )
+}
+export default Header
